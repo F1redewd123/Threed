98ee5ec66e43ac10b00b75b91dc8bef295afe050
FireDude123 <153016828+F1redewd123@users.noreply.github.com>
2026-02-11 07:52:19 -0500

    Update Shader.js

--- Shader.js
+++ Shader.js
@@ -1,6 +1,11 @@
 export default class Shader {
   program = {};
+  
   createFragShader(frag) {
     this.program["frag"] = new Function("data", frag);
+  }
+
+  createVertShader(vert) {
+    this.program["vert"] = new Function("data", vert);
   }
 }
