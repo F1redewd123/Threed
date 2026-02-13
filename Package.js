167738dcced9b1b4193bd7dfe74796c953579d32
FireDude123 <153016828+F1redewd123@users.noreply.github.com>
2026-02-13 08:54:22 -0500

    Delete Package.js

--- Package.js
+++
@@ -1,7 +0,0 @@
-export async function genPrimitives() {
-  var vec2 = await import("https://f1redewd123.github.io/Threed/Vector2.js");
-  var vec3 = await import("https://f1redewd123.github.io/Threed/Vector3.js");
-  var vec4 = await import("https://f1redewd123.github.io/Threed/Vector4.js");
-
-  return [ vec2, vec3, vec4 ];
-}
