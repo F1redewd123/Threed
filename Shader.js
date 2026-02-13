b40ee653c746f7965fdd59ab488916272a142bad
FireDude123 <153016828+F1redewd123@users.noreply.github.com>
2026-02-13 07:58:49 -0500

    Update Shader.js

--- Shader.js
+++ Shader.js
@@ -1,7 +1,3 @@
-import Vector2 from "https://f1redewd123.github.io/Threed/Vector2.js";
-import Vector3 from "https://f1redewd123.github.io/Threed/Vector2.js";
-import Vector4 from "https://f1redewd123.github.io/Threed/Vector2.js";
-
 export default class Shader {
   program = {};
   
