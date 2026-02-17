b97e9dcd8f9acf92367d9fb66d5de8fb013f3e6e
FireDude123 <153016828+F1redewd123@users.noreply.github.com>
2026-02-17 11:29:07 -0500

    Update Shader.js

--- Shader.js
+++ Shader.js
@@ -1,11 +1,21 @@
+import Vector2 from "https://f1redewd123.github.io/Threed/Vector2.js";
+import Vector3 from "https://f1redewd123.github.io/Threed/Vector3.js";
+import Vector4 from "https://f1redewd123.github.io/Threed/Vector4.js";
+
 export default class Shader {
   program = {};
+
+  constructor() {
+    window.Vector2 = Vector2;
+    window.Vector3 = Vector3;
+    window.Vector4 = Vector4;
+  }
   
   createFragmentShader(frag) {
     this.program["frag"] = new Function("data", frag);
   }
 
-  async createVertexShader(vert) {
+  createVertexShader(vert) {
     this.program["vert"] = new Function("data", vert + " return data;");
   }
 }
