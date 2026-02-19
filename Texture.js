40d38e671eee25b1c75475e85938646654a035b9
FireDude123 <153016828+F1redewd123@users.noreply.github.com>
2026-02-19 18:28:48 -0500

    Update Texture.js

--- Texture.js
+++ Texture.js
@@ -6,7 +6,7 @@ export default class Texture {
   #dataBuffer = [];
   filterMode;
   
-  static fromURL(url, filterMode = 1) {
+  static fromURL(url, filterMode = 0) {
     return new Promise((r, e) => {
       var img = new Image();
       img.crossOrigin = "Anonymous";
@@ -16,9 +16,7 @@ export default class Texture {
         tex.width = this.naturalWidth;
         tex.height = this.naturalHeight;
         tex.filterMode = filterMode;
-        for (var i = 0; i < data.length; i += 4) {
-          tex.#dataBuffer.push(new Vector4(data[i], data[i + 1], data[i + 2], data[i + 3]));
-        }
+        tex.#dataBuffer = data;
         r(tex);
       };
       img.onerror = () => { e("Texture could not be loaded"); };
@@ -40,21 +38,6 @@ export default class Texture {
       return -1;
     if (this.filterMode == 0) {
       return this.#dataBuffer[Math.floor(p.x * (this.width - 1)) + Math.floor(p.y * (this.height - 1)) * this.width];
-    } else if (this.filterMode == 1) {
-      var lerp = function(a, b, t) {
-        return a + (b - a) * t;
-      };
-      
-      var f0 = this.#dataBuffer[Math.floor(p.x * (this.width - 1)) + Math.floor(p.y * (this.height - 1)) * this.width];
-      var f1 = this.#dataBuffer[Math.ceil(p.x * (this.width - 1)) + Math.ceil(p.y * (this.height - 1)) * this.width];
-      var f2 = this.#dataBuffer[p.x * (this.width - 1) + p.y * (this.height - 1) * this.width].sub(this.#dataBuffer[Math.floor(p.x * (this.width - 1)) + Math.floor(p.y * (this.height - 1)) * this.width]);
-      
-      return new Vector4(
-        lerp(f0.x, f1.x, f2.x),
-        lerp(f0.y, f1.y, f2.y),
-        lerp(f0.z, f1.z, f2.z),
-        lerp(f0.w, f1.w, f2.w)
-      );
     }
   }
 }
