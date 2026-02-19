f731265baf53ab8b63be5ab3ea92ee0a35ee18f1
FireDude123 <153016828+F1redewd123@users.noreply.github.com>
2026-02-19 07:57:47 -0500

    Update Shader.js

--- Shader.js
+++ Shader.js
@@ -1,6 +1,7 @@
 import Vector2 from "https://f1redewd123.github.io/Threed/Vector2.js";
 import Vector3 from "https://f1redewd123.github.io/Threed/Vector3.js";
 import Vector4 from "https://f1redewd123.github.io/Threed/Vector4.js";
+import Texture from "https://f1redewd123.github.io/Threed/Texture.js";
 
 export default class Shader {
   program = {};
@@ -9,6 +10,7 @@ export default class Shader {
     window.Vector2 = Vector2;
     window.Vector3 = Vector3;
     window.Vector4 = Vector4;
+    window.Texture = Texture;
   }
   
   createFragmentShader(frag) {
